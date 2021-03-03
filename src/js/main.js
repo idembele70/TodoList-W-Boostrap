dcc906e2ea73ffa468de9e65ac63ba5793a71183
idembele70 <idembele70@gmail.com>
2021-03-03 22:05:30 +0100

    bordure et ombre du input et du btn add todo

--- src/js/main.js
+++ src/js/main.js
@@ -2,19 +2,19 @@
 // Selector
 const todoTitle = document.querySelector('.todo-title');
 const inputAddTodo = document.getElementById('input-add-todo');
+const btnAddTodo = document.getElementsByClassName('btn-add-todo')[0];
 
 // insert in Document
 inputAddTodo.addEventListener('focus',()=>{
         inputAddTodo.classList.add('border-dark');
-        
         inputAddTodo.classList.add('shadow-sm');
-},true)
+
+},false)
 
 inputAddTodo.addEventListener('blur',()=>{
-        inputAddTodo.classList.remove('shadow-sm');
-
         inputAddTodo.classList.remove('border-dark');
-},true)
+        inputAddTodo.classList.remove('shadow-sm');
+},false)
 
 
 
