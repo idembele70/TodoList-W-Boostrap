d770f8fd90373d432d181d5a6abab2709abf7c75
idembele70 <idembele70@gmail.com>
2021-03-13 20:18:42 +0100

    set items checked

--- src/js/main.js
+++ src/js/main.js
@@ -6,9 +6,13 @@ const btnAddTodo = document.getElementsByClassName('btn-add-todo')[0];
 const todoList = document.querySelector('.todolist');
 var todo = document.getElementsByClassName('todo');
 const closed = document.getElementsByClassName('close');
+const todoLine = document.getElementsByClassName('line')
 
 let task = '';
-// insert in Document
+
+// functions
+
+// Bootsrap Event
 inputAddTodo.addEventListener('focus', () => {
         inputAddTodo.classList.add('border-dark');
         inputAddTodo.classList.add('shadow-sm');
@@ -20,27 +24,32 @@ inputAddTodo.addEventListener('blur', () => {
         inputAddTodo.classList.remove('shadow-sm');
 }, false);
 
-// functions
-
 //Add todo
 btnAddTodo.onclick = function () {
         if (!inputAddTodo.value) {
                 alert("Veuillez inserer une tache");
         } else {
-                todoList.innerHTML +=
-                        ` <p class="col-12 m-0 py-2 mb-2 border border-dark position-relative">
-        <span class="todo-items">${inputAddTodo.value}</span>
-        <button type="button" class="close btn btn-danger position-absolute end-0 bottom-0 h-100 px-3 shadow-none"><i class="fas fa-times"></i></button>
-        </p>`
+                addTask()
                 inputAddTodo.value = '';
         }
-        deleteItems()
+        deleteItems();
+        itemChecked();
 }
 
+// ajouter une nouvelle todo
+function addTask() {
+        todoList.innerHTML +=
+                ` <p class="line col-12 m-0 py-2 mb-2 border border-dark position-relative">
+<span class="todo-items">${inputAddTodo.value}</span>
+<button type="button" class="close btn btn-danger position-absolute end-0 bottom-0 h-100 px-3 shadow-none"><i class="fas fa-times"></i></button>
+</p>`
+}
+
+// supprimer un items
 function deleteItems() {
         let i;
-        for (i = 0; i < closed.length ; i++) {
-                closed[i].onclick = function() {   
+        for (i = 0; i < closed.length; i++) {
+                closed[i].onclick = function () {
                         const parent = this.parentElement;
                         parent.style.opacity = 0;
                         setTimeout(() => {
@@ -50,6 +59,17 @@ function deleteItems() {
         }
 }
 
+// supprimer tout les items
 function deleteAll() {
         todoList.innerHTML = "";
+}
+
+// items completé
+function itemChecked() {
+        for (const col of todoLine) {
+                col.onclick = () => {
+                        const span = col.getElementsByTagName('span')[0];
+                        span.classList.toggle("upper");
+                }
+        }
 }
