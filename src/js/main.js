30e673a1d526200a20c9b8c822dcd783e4d05bad
idembele70 <idembele70@gmail.com>
2021-03-14 21:00:18 +0100

    sa bug

--- src/js/main.js
+++ src/js/main.js
@@ -5,9 +5,8 @@ const inputAddTodo = document.getElementById('input-add-todo');
 const btnAddTodo = document.getElementsByClassName('btn-add-todo')[0];
 const todoList = document.querySelector('.todolist');
 var todo = document.getElementsByClassName('todo');
+const todoLine = document.getElementsByClassName('line');
 const closed = document.getElementsByClassName('close');
-const todoLine = document.getElementsByClassName('line')
-
 let task = '';
 
 // functions
@@ -36,20 +35,24 @@ btnAddTodo.onclick = function () {
         itemChecked();
 }
 
+
 // ajouter une nouvelle todo
 function addTask() {
         todoList.innerHTML +=
-                ` <p class="line col-12 m-0 py-2 mb-2 border border-dark position-relative">
-<span class="todo-items">${inputAddTodo.value}</span>
-<button type="button" class="close btn btn-danger position-absolute end-0 bottom-0 h-100 px-3 shadow-none"><i class="fas fa-times"></i></button>
-</p>`
+                `<div class="line col-12 m-0 mb-2 border border-dark p-0 d-flex justify-content-between align-items-center">
+                <span class="todo-items w-100 h-100 d-flex align-items-center">
+                   <p class="m-0 "> ${inputAddTodo.value}</p>
+                </span>
+                <button type="button" class="closef btn btn-danger px-3 shadow-none"><i class="fas fa-times"></i></button>
+            </div>`
 }
 
 // supprimer un items
 function deleteItems() {
         let i;
-        for (i = 0; i < closed.length; i++) {
-                closed[i].onclick = function () {
+        for (i = 0; i < close.length; i++) {
+                closed[i].onclick = function() {
+                        console.log('ok');
                         const parent = this.parentElement;
                         parent.style.opacity = 0;
                         setTimeout(() => {
@@ -69,7 +72,7 @@ function itemChecked() {
         for (const col of todoLine) {
                 col.onclick = () => {
                         const span = col.getElementsByTagName('span')[0];
-                        span.classList.toggle("upper");
+                        span.classList.toggle("taskChecked");
                 }
         }
 }
