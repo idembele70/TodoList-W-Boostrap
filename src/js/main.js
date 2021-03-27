5f9e5edc93e895aa0fddd49fe67023e2d64d784f
idembele70 <idembele70@gmail.com>
2021-03-27 23:24:06 +0100

    tentative de supprimer

--- src/js/main.js
+++ src/js/main.js
@@ -6,7 +6,7 @@ const btnAddTodo = document.getElementsByClassName('btn-add-todo')[0];
 const todoList = document.querySelector('.todolist');
 var todo = document.getElementsByClassName('todo');
 const todoLine = document.getElementsByClassName('line');
-const closed = document.getElementsByTagName('p');
+const closed = document.getElementsByClassName('todo-items');
 let task = '';
 
 // functions
@@ -23,35 +23,50 @@ inputAddTodo.addEventListener('blur', () => {
         inputAddTodo.classList.remove('shadow-sm');
 }, false);
 
+
+const btn = createElems('button');
+btn.className = 'close btn btn-danger px-1 shadow-none col-1';
+btn.innerHTML = '<i class="fas fa-times"></i>';
+for (let i = 0; i < closed.length; i++) {
+        closed[i].appendChild(btn);
+        console.log(i);      
+}
+
+// todoList.lastChild.appendChild(btn);
 //Add todo
 btnAddTodo.onclick = function () {
         if (!inputAddTodo.value) {
                 alert("Veuillez inserer une tache");
         } else {
-                addTask()
-                inputAddTodo.value = '';
-                console.log(closed);
-        }
-        itemChecked();
-}
 
+             
 
-// ajouter une nouvelle todo
-function addTask() {
-        todoList.innerHTML +=
-                `   <div class="col-12 m-0 mb-2 border  d-flex align-items-center border-dark p-0">
+                todoList.innerHTML +=
+                        `   <div class="col-12 m-0 mb-2 border  d-flex align-items-center border-dark p-0">
                 <span class="col todo-items p-0 d-table h-100">
                    <p class="col m-0 d-table-cell align-middle "> ${inputAddTodo.value}</p>
                 </span>
-                <button type="button" class="close btn btn-danger px-1 shadow-none col-1"><i class="fas fa-times"></i></button>
-            </div>`
+                </div>`
+               
+
+
+                inputAddTodo.value = '';
+                console.log(closed);
+                let i;
+                for (i = 0; i < closed.length; i++) {
+                        closed[i].onclick = () => {
+                                
+                        }
+                }
+        }
 }
+
 
 // supprimer un items
 let i;
-for (i =0; i< closed.length;i++) {
-        closed[i].onclick = ()=> {
-               console.log(closed[i]);
+for (i = 0; i < todoList.length; i++) {
+        todoList[i].onclick = () => {
+                console.log(closed[i]);
         }
 }
 
@@ -68,4 +83,8 @@ function itemChecked() {
                         span.classList.toggle("taskChecked");
                 }
         }
+}
+
+function createElems(elems) {
+        return document.createElement(`${elems}`);
 }
