d2debe3ce98d3db77804d1cdb5fa33d89e68afc6
idembele70 <idembele70@gmail.com>
2021-04-01 23:01:59 +0200

    SUPPRESSION ajout de nouveau style

--- src/js/main.js
+++ src/js/main.js
@@ -5,9 +5,6 @@ const inputAddTodo = document.getElementById('input-add-todo');
 const btnAddTodo = document.getElementsByClassName('btn-add-todo')[0];
 const todoList = document.querySelector('.todolist');
 var todo = document.getElementsByClassName('todo');
-const todoLine = document.getElementsByClassName('line');
-const closed = document.getElementsByClassName('todo-items');
-let task = '';
 
 // functions
 
@@ -23,51 +20,27 @@ inputAddTodo.addEventListener('blur', () => {
         inputAddTodo.classList.remove('shadow-sm');
 }, false);
 
-
-const btn = createElems('button');
-btn.className = 'close btn btn-danger px-1 shadow-none col-1';
-btn.innerHTML = '<i class="fas fa-times"></i>';
-for (let i = 0; i < closed.length; i++) {
-        closed[i].appendChild(btn);
-        console.log(i);      
-}
-
-// todoList.lastChild.appendChild(btn);
 //Add todo
 btnAddTodo.onclick = function () {
         if (!inputAddTodo.value) {
                 alert("Veuillez inserer une tache");
         } else {
-
-             
-
                 todoList.innerHTML +=
-                        `   <div class="col-12 m-0 mb-2 border  d-flex align-items-center border-dark p-0">
-                <span class="col todo-items p-0 d-table h-100">
-                   <p class="col m-0 d-table-cell align-middle "> ${inputAddTodo.value}</p>
-                </span>
+                        `<div class="todo-line position-relative col-12 m-0 mb-2 d-flex align-items-center p-0">
+                <span onclick="itemChecked(this)" class="taskNotChecked col todo-items p-0 d-table h-100">
+                   <p class="col m-0 ms-1 d-table-cell align-middle ">${inputAddTodo.value}</p>
+                   </span>
+                   <button type="button" onclick="closeTodo(this.parentElement)" class="close btn btn-danger px-1 shadow-none rounded-0 col-1"><i class="fas fa-times"></i></button>
                 </div>`
-               
-
-
                 inputAddTodo.value = '';
-                console.log(closed);
-                let i;
-                for (i = 0; i < closed.length; i++) {
-                        closed[i].onclick = () => {
-                                
-                        }
-                }
         }
 }
-
-
 // supprimer un items
-let i;
-for (i = 0; i < todoList.length; i++) {
-        todoList[i].onclick = () => {
-                console.log(closed[i]);
-        }
+function closeTodo(todo) {
+        todo.style.opacity = 0;
+        setTimeout(() => {
+                todoList.removeChild(todo)
+        }, 200);
 }
 
 // supprimer tout les items
@@ -76,13 +49,9 @@ function deleteAll() {
 }
 
 // items completé
-function itemChecked() {
-        for (const col of todoLine) {
-                col.onclick = () => {
-                        const span = col.getElementsByTagName('span')[0];
-                        span.classList.toggle("taskChecked");
-                }
-        }
+function itemChecked(e) {
+        
+        return e.classList.toggle('taskChecked'), e.classList.toggle('taskNotChecked');
 }
 
 function createElems(elems) {
